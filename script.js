f2d175030c996ce8b93260e44277cd8c134bb2df
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-09-18 13:18:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 // global angle
 let angle = 0;
 
@@ -8,6 +7,5 @@ const line = document.getElementById('line');
 // update function runs every 20ms and increases angle by 2 degrees
 setInterval(() => {
   angle += 2;
-  // keep the translate so the element stays centered, and apply rotation (deg)
   line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
 }, 20);
